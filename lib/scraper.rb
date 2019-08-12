a706e5588c635e8fa2bce2209a2e357adfce9d0a
Shayla White <shayladwhite1@gmail.com>
2019-08-12 03:08:39 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,5 +4,5 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(open("http://flatironschool.com/"))
-doc.css(".site-header__hero__headline").text
+doc.css(".site-header__hero__headline").text.strip
 
